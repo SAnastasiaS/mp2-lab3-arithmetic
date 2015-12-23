02da5c26c3fcd3e3bfc49d23f0aab663d038e7be
SAnastasiaS <a.samarina1997@gmail.com>
2015-12-23 19:39:56 +0300

    Same

---
+++ include/RPN.h
@@ -0,0 +1,22 @@
+#include "stack.h"
+#include <string>
+#include <cstring>
+#include <cctype>
+#include <sstream> 
+#include <cmath>
+using namespace std;
+
+class RPN//Reverse Polish Notation
+{
+public:
+	static double Calculate(string input);
+	
+		static string GetExpression(string input);
+			static double Counting(string input);
+private:
+
+
+	static bool IsDelimeter(char c);
+	static bool IsOperator(char ñ);
+	static short unsigned int GetPriority(char s);
+};
